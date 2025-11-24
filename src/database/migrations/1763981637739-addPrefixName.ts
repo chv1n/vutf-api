fa713393d111ae6a24f7d0f528e0218be12b405a
chv1n <cendozoden@gmail.com>
2025-11-24 18:03:10 +0700

    fix : prefix migration 2

--- src/database/migrations/1763981637739-addPrefixName.ts
+++ src/database/migrations/1763981637739-addPrefixName.ts
@@ -4,7 +4,7 @@ export class AddPrefixName1763981637739 implements MigrationInterface {
     name = 'AddPrefixName1763981637739'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "student" ADD "prefix_name" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "student" ADD "prefix_name" character varying NOT NULL DEFAULT ''`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
