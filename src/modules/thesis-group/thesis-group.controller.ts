2bd726bc06f9665a339d52f056fd61689c78e563
chv1n <cendozoden@gmail.com>
2026-01-07 19:56:51 +0700

    feat(thesis-group): implement update and get thesis group by id

--- src/modules/thesis-group/thesis-group.controller.ts
+++ src/modules/thesis-group/thesis-group.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Param, Req, UseGuards } from '@nestjs/common';
 import { ThesisGroupService } from './thesis-group.service';
 import { CreateThesisGroupDto } from './dto/create-thesis-group.dto';
+import { UpdateThesisDto } from '../thesis/dto/update-thesis.dto';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('thesis-group')
 export class ThesisGroupController {
-  constructor(private readonly thesisGroupService: ThesisGroupService) {}
-
+  constructor(private readonly thesisGroupService: ThesisGroupService) { }
 
   @UseGuards(AuthGuard('jwt'))
   @Post()
@@ -16,6 +16,20 @@ export class ThesisGroupController {
   ) {
     return this.thesisGroupService.createFullThesis(createThesisGroupDto, req.user.userId);
   }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put('/:groupId/thesis')
+  async updateThesisInfo(
+    @Req() req,
+    @Param('groupId') groupId: string,
+    @Body() dto: UpdateThesisDto,
+  ) {
+    return this.thesisGroupService.updateThesisInfo(req.user.userId, groupId, dto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/:groupId')
+  async getThesisGroupById(@Param('groupId') groupId: string) {
+    return this.thesisGroupService.getThesisGroupById(groupId);
+  }
 }
-
-
