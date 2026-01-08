4e4cdf77734e0b3b552f650cb39e3d94eff842c5
chv1n <cendozoden@gmail.com>
2026-01-08 17:30:54 +0700

    fix : generate and run migration submisstion table

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -7,7 +7,7 @@ import { InspectionRoundModule } from './inspection_round/inspection_round.modul
 import { ThesisGroupModule } from './thesis-group/thesis-group.module';
 
 import { AnnouncementsModule } from './announcements/announcements.module';
-import { SubmissionsModule } from '../submissions/submissions.module';
+import { SubmissionsModule } from './submissions/submissions.module';
 
 import { ThesisModule } from './thesis/thesis.module';
 import { GroupMemberModule } from './group-member/group-member.module';
